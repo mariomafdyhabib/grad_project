2cb490ff18d44533d7c37d4bc6902367551f7d7c
mariomafdyhabib <mariomafdy333@gmail.com>
2025-05-28 21:12:56 +0300

    terraform

---
+++ terraform/storage.tf
@@ -0,0 +1,10 @@
+resource "kubernetes_storage_class" "ebs_sc" {
+  metadata {
+    name = "ebs-sc"
+  }
+  storage_provisioner = "ebs.csi.aws.com"
+  volume_binding_mode = "WaitForFirstConsumer"
+  parameters = {
+    type = "gp3"
+  }
+}
